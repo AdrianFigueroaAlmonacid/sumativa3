a2fe465ccad0264346b2ebf37dd61094dc9dc318
AdrianFigueroaAlmonacid <adri.figueroa@duocuc.cl>
2024-04-25 14:43:29 -0400

    se agrega la funcionalidad GET', 'PUT', 'PATCH', 'DELETE en la api creada

--- sumativa3/rest_api/urls.py
+++ sumativa3/rest_api/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('series/', views.lista_series, name='lista_series'),
+    path('series/<id>', views.vista_serie, name='vista_serie'),
+
 ]
--- sumativa3/rest_api/views.py
+++ sumativa3/rest_api/views.py
@@ -4,6 +4,8 @@ from .serializers import seriesSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework import status
 
 
 @csrf_exempt
@@ -14,3 +16,40 @@ def lista_series(request):
         serializer = seriesSerializer(serie, many=True)
 
         return Response(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parser(request)
+        serializer = seriesSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            print('error', serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def vista_serie(request, id):
+    try:
+        serie = series.objects.get(id=id)
+    except serie.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = seriesSerializer(serie)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT' or request.method == 'PATCH':
+        serializer = seriesSerializer(serie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            print('error', serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        serie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
